ec9b8d8f7c16a3f6f5af7277141fc9c074471d8b
Drew C <jayedeem@gmail.com>
2023-01-05 07:10:08 -0800

    changes

--- .config/nvim/lua/madflava/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/madflava/plugins/lsp/lspconfig.lua
@@ -60,7 +60,7 @@ for type, icon in pairs(signs) do
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
+-- vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#1f2335]])
 vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]])
 
 local border = {
